fe9ab984345ad0b4a42bb61142d0a3e09b1b84b9
Boki23m <boki23usa@yahoo.com>
2019-03-10 13:28:23 -0700

    server.js logic added

--- server.js
+++ server.js
@@ -22,3 +22,45 @@ app.get("/", function (req, res) {
 app.get("/api/friends", function (req, res) {
     return res.json(friendsList.friends);
 });
+
+
+app.post("/api/friends", function (req, res) {
+    //handle incoming surver results
+    var newFriend = req.body;
+    //compatability logic
+    console.log(newFriend)
+    friendsList.friends.push(newFriend);
+    var lowestScore = 100;
+    var newBestFriendName;
+    var newBestFriendPic;
+    for (i = 0; i < friendsList.friends.length - 1; i++) {
+        var totalDifference = 0;
+        for (j = 0; j < friendsList.friends[i].scores.length; j++) {
+            var compareTo = friendsList.friends.length - 1;
+            var newestMember = friendsList.friends[compareTo]
+            totalDifference = totalDifference +  Math.abs(friendsList.friends[i].scores[j] - newestMember.scores[j]);
+            // console.log("total difference: " + totalDifference);
+        }
+        if (totalDifference < lowestScore) {
+            newBestFriendName = friendsList.friends[i].name;
+            newBestFriendPic = friendsList.friends[i].photo;
+            lowestScore = totalDifference;
+            console.log("Lowest score: " + lowestScore + " points from " + newBestFriendName)
+
+        } else {
+            console.log(friendsList.friends[i].name + " is not a good match.")
+        }
+    }
+    
+    console.log("Your score was " + totalDifference + " points from " + newBestFriendName)
+    res.json({
+        newFriend: newBestFriendName,
+        newFriendPic: newBestFriendPic
+    })
+
+})
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
