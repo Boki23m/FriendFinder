573e054e895e29c9ca1a83020ce4324c5e75d58e
Boki23m <boki23usa@yahoo.com>
2019-03-10 14:06:51 -0700

    friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,58 @@
 var friends = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "Sheldon",
+    photo: "https://the-big-bang-theory.com/images/uploads/5/rs_3463ec66edb459bd1dd.jpg",
     scores: [
         5,
+        5,
+        5,
+        5,
         1,
+        1,
+        5,
+        5,
+        1,
+        1
+    ],
+    name: "Raj",
+    photo: "https://the-big-bang-theory.com/images/uploads/2/rs_35619e852e696869058.jpg",
+    scores: [
+        5,
+        5,
+        1,
+        5,
+        1,
+        5,
         4,
+        3,
+        4,
+        3
+    ],
+    name: "Penny",
+    photo: "https://the-big-bang-theory.com/images/uploads/3/rs_304c3dd3b937df81cac.jpg",
+    scores: [
+        3,
+        2,
+        6,
         4,
         5,
         1,
         2,
         5,
         4,
-        1
-    ],
+        1],
+    name: "Leonard",
+    photo: "https://the-big-bang-theory.com/images/uploads/2/rs_1365ce5b16123fd10b8.jpg",
+    scores: [
+        5, 
+        1, 
+        4, 
+        4, 
+        5, 
+        1, 
+        2, 
+        5, 
+        4, 
+        1],
 }];
 
 module.exports = { friends: friends }
